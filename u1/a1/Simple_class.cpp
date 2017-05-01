0313226b90559431484b95d1c07bc245fca7077e
Nils Hanke <nils.hanke@ruhr-uni-bochum.de>
2017-05-01 17:42:07 +0200

    Übung 1 - Aufgabe 1

---
+++ u1/a1/Simple_class.cpp
@@ -0,0 +1,7 @@
+#include "Simple_class.hpp"
+Simple_class::Simple_class(int input) {
+    integer = input;
+}
+void Simple_class::set(int input) {
+    integer = input;
+}
---
+++ u1/a1/Simple_class.hpp
@@ -0,0 +1,8 @@
+class Simple_class{
+private:
+    int integer;
+public:
+    Simple_class(int input);
+    int get() {return integer;};
+    void set(int input);
+};
