294a13b3004ad2597a1ceaf4342a6b1320bea4cd
Nils Hanke <nils.hanke@ruhr-uni-bochum.de>
2017-05-24 12:58:41 +0200

    Übung 2 Aufgabe 3 - Compilefehler behoben

--- u2/a3/tests.cpp
+++ u2/a3/tests.cpp
@@ -1,7 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-// This include causes an error because Weekday_statistics is already included in count_13th.hpp
-// #include "Weekday_statistics.hpp"
+#include "Weekday_statistics.hpp"
 #include "count_13th.hpp"
 
 TEST_CASE("Creating a class")
