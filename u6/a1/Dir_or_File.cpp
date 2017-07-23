4d498f26c837c851cee8466c3b8995b438b8f5f7
Nils Hanke <nils.hanke@ruhr-uni-bochum.de>
2017-07-23 16:49:14 +0200

    Übung 6 Aufgabe 1 - Konstruktur repariert + insert() hinzugefügt

--- u6/a1/Dir_or_File.cpp
+++ u6/a1/Dir_or_File.cpp
@@ -3,29 +3,23 @@
 
 Dir_or_File::Dir_or_File(std::string name)
 {
-	// constructor stuff goes here
-	// make sure that is_file is set according to the '/' rules -> make a check here!
-	is_file = name.back() != '/';
+	this->name = name;
+	this->is_file = name.back() != '/';
 }
 
 
 std::vector<std::string> Dir_or_File::split_after_slash(std::string full_path)
 {
-	// take care of '/'
-	// check what comes after a slash and decide what to do according to what you find
-	// so iterate through your string and find slashes
-	// and then decide what to do...
-
 	std::vector<std::string> ret;
-	// iteration goes here
+
 	int position_after_last_slash = 0;
  	for(int i=0; i < full_path.length(); i++)
 	{
 		if (full_path[i] == '/')
 		{
-			if (i == 0)
+			if (i == 0) // workaround for root path, because substr(0,0) doesn't work
 			{
-				ret.push_back("/");
+				ret.emplace_back("/");
 			}
 			else
 			{
@@ -41,14 +35,30 @@ std::vector<std::string> Dir_or_File::split_after_slash(std::string full_path)
 
 void Dir_or_File::insert(std::vector<std::string> names)
 {
-	// now comes the insertion of new entries
-	// again make sure to distinguish between files and directories
-	for (int i=0; i < names.size(); i++)
+	if (names.empty()) // since we recursively call this function again, we have to abort if we have nothing to insert
+		return;
+
+	// pick the current directory or file to work with, than move the rest of the path up for our next iterator
+	std::string current_name = names[0]; // save the dir or file we want to work with as current_name
+	names.erase(names.begin()); // remove the just-before saved entry from "names" so we can use "names" for the recursive calls
+	// go through "entries" to search for equal (case 1) or bigger entries (case 2)
+	for (int i=0; i<entries.size(); i++)
 	{
-		if (std::binary_search(this->entries.begin(), this->entries.end(), names[i])) // if the dir or file already exists
+		// case 1: If we find the same dir or file, recursively call the insert function again with the reduced path
+		if (current_name == entries[i]->name)
 		{
-			continue;
+			entries[i]->insert(names);
+			return;
+		}
+		// case 2: If we find an entry with a "higher" letter, we insert before this one and recursively call the insert again on the newly inserted object for the rest of "names"
+		if (current_name < entries[i]->name)
+		{
+			entries.insert(entries.begin()+ i, new Dir_or_File(current_name)); // workaround: entries.begin() + i because we need to provide an iterator instead of just an index
+			entries[i]->insert(names);
+			return;
 		}
 	}
+	// case 3: if we find no equal or "bigger" entry, than we need to insert at the end of "entries"
+	entries.emplace_back(new Dir_or_File(current_name));
+	entries[entries.size() - 1]->insert(names);
 }
-
